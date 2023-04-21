7b743de8bb534ed2db9d1574c19362da8b6ac9ae
Harman Singh <harman.singh@student.howest.be>
2023-04-21 22:15:35 +0200

    Turned project into a PWA + PWA optimized it

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,23 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+
+import { useEffect } from "react";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+	useEffect(() => {
+		if ("serviceWorker" in navigator) {
+			window.addEventListener("load", () => {
+				navigator.serviceWorker
+					.register("/sw.js")
+					.then((registration) =>
+						console.log("Service worker registered")
+					)
+					.catch((error) =>
+						console.error("Error registering service worker", error)
+					);
+			});
+		}
+	}, []);
+
+	return <Component {...pageProps} />;
 }
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,22 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+	return (
+		<Html lang="en">
+			<Head>
+				<link rel="manifest" href="/manifest.json" />
+				<meta name="theme-color" content="#000000" />
+				<meta
+					name="description"
+					content="Harman Singh's Developer Portfolio"
+				/>
+				<link rel="icon" href="/favicon.ico" />
+				<link rel="apple-touch-icon" href="/NAME_192.png" />
+			</Head>
+			<body>
+				<Main />
+				<NextScript />
+			</body>
+		</Html>
+	);
 }
