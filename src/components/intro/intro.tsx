a8df16da94e144ace9b9b0cfe65c69edac4ace33
Harman Singh <harman.singh@student.howest.be>
2023-04-21 13:03:11 +0200

    Finish INTRO completely

--- src/components/intro/intro.tsx
+++ src/components/intro/intro.tsx
@@ -1,26 +1,39 @@
-import Nav from "./nav"
+import Nav from "./nav";
+
+import { ArrowDownIcon } from "@heroicons/react/24/outline";
 
 export default function Intro() {
-    return (
-        <>
-            <div className="bg-main text-tan min-h-screen px-8 md:px-16 lg:px-36">
-                <Nav/>
+	return (
+		<>
+			<div className="bg-main text-tan min-h-screen px-8 md:px-16 lg:px-28">
+				<Nav />
 
-                <div className="mt-36">
-                    <h2 className="text-2xl">Hello, I&apos;m Harman</h2>
-                    <p className=" text-5xl sm:text-6xl md:text-6xl lg:text-7xl xl:text-8xl font-dm">I create marketing <br/> strategies for your <br/> business that get <br/> results.</p>
-                </div>
+				<div>
+					{/* LEFT PART */}
+					<div className="mt-36">
+						<h2 className="text-2xl">Hello, I&apos;m Harman</h2>
+						<p className=" text-5xl sm:text-6xl md:text-6xl lg:text-7xl xl:text-8xl font-dm">
+							I create marketing <br /> strategies for your <br />{" "}
+							business that get <br /> results.
+						</p>
+					</div>
 
-                <div>
-                    {/* SCROLL FOR MORE */}
-                    <a href="Continue"></a>
-                </div>
+					<div className="mt-12 flex gap-4 items-center">
+						{/* SCROLL FOR MORE */}
+						<div className="border rounded-3xl border-accent p-3">
+							<ArrowDownIcon className="h-4" />
+						</div>
+						<a href="" className="text-xl font-light">
+							Scroll for more
+						</a>
+					</div>
+				</div>
 
-                {/* SCROLL CHECK
+				{/* SCROLL CHECK
                 <div className="bg-blue-400 min-h-screen">BLUE</div>
                 <div className="bg-green-400 min-h-screen">GREEN</div>
                 <div className="bg-red-400 min-h-screen">RED</div> */}
-            </div>
-        </>
-    )
+			</div>
+		</>
+	);
 }
--- src/components/intro/nav.tsx
+++ src/components/intro/nav.tsx
@@ -1,17 +1,17 @@
 export default function Nav() {
 	return (
-		<div className=" sticky top-0 z-10 pt-12 text-white flex justify-between flex-col lg:flex-row">
+		<div className=" sticky top-0 z-10 pt-12 text-white flex justify-between flex-col md:flex-row md:items-center">
             <div>
                 <h1 className="font-dm text-tan text-3xl">Harman.</h1>
-                <div className="flex gap-4">
+                <div className="flex mt-4 sm:mt-0 gap-4 flex-wrap">
                     <a className="text-greenish" href="#About">About</a>
                     <a className="text-greenish" href="#Services">Services</a>
                     <a className="text-greenish" href="#Journal">Journal</a>
                     <a className="text-greenish" href="#Contact">Contact</a>
                 </div>
             </div>
-            <div className="hidden lg:inline">
-                <a className="bg-accent px-8 py-6 rounded-md text-main text-lg font-medium" href="#Contact">Let&apos;s work together</a>
+            <div className="hidden md:inline">
+                <a className="bg-accent px-8 py-4 rounded-md text-main text-md font-medium" href="#Contact">Contact Me</a>
             </div>
 		</div>
 	);
