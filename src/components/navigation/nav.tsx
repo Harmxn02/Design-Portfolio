2f5ac8e9c2a4e8b73d884a6d63db9ba63263f94b
Harman Singh <harman.singh@student.howest.be>
2023-04-23 20:01:27 +0200

    Adjust nav bar on mobile views (too big and same colour)

--- src/components/navigation/nav.tsx
+++ src/components/navigation/nav.tsx
@@ -1,9 +1,9 @@
 export default function Nav() {
 	return (
-		<div className="px-8 md:px-16 lg:px-28 drop-shadow-sm bg-main sticky py-8 top-0 z-10 pt-12 text-white flex justify-between flex-col md:flex-row md:items-center">
+		<div className="px-8 md:px-16 lg:px-28 drop-shadow-sm bg-main_2 sm:bg-main sticky py-4 sm:py-8 top-0 z-10 sm:pt-12 text-white flex justify-between flex-col md:flex-row md:items-center">
             <div>
-                <h1 className="font-dm text-tan text-3xl">Harman.</h1>
-                <div className="flex mt-4 sm:mt-0 gap-4 flex-wrap">
+                <h1 className="font-dm text-center sm:text-left text-tan text-3xl">Harman.</h1>
+                <div className="hidden sm:flex mt-4 sm:mt-0 gap-4 flex-wrap">
                     <a className="text-greenish hover:text-white transition duration-500" href="#ABOUT">About</a>
                     <a className="text-greenish hover:text-white transition duration-500" href="#Services">Services</a>
                     <a className="text-greenish hover:text-white transition duration-500" href="#Journal">Journal</a>
