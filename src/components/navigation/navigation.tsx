9d2e96158e648ec61c3a247a9b7d55aa2ed6b0e5
Harman Singh <harman.singh@student.howest.be>
2023-04-30 15:35:42 +0200

    Change nav colour if not at the top
    (helps with visibility)

--- src/components/navigation/navigation.tsx
+++ src/components/navigation/navigation.tsx
@@ -1,66 +1,33 @@
-import { Fragment, useState } from "react";
-import { Dialog, Disclosure, Popover, Transition } from "@headlessui/react";
-import {
-	ArrowPathIcon,
-	Bars3Icon,
-	ChartPieIcon,
-	CursorArrowRaysIcon,
-	FingerPrintIcon,
-	SquaresPlusIcon,
-	XMarkIcon,
-} from "@heroicons/react/24/outline";
-import {
-	ChevronDownIcon,
-	PhoneIcon,
-	PlayCircleIcon,
-} from "@heroicons/react/20/solid";
+import { useState, useEffect } from "react";
+import { Dialog, Popover } from "@headlessui/react";
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
 
-const products = [
-	{
-		name: "Analytics",
-		description: "Get a better understanding of your traffic",
-		href: "#",
-		icon: ChartPieIcon,
-	},
-	{
-		name: "Engagement",
-		description: "Speak directly to your customers",
-		href: "#",
-		icon: CursorArrowRaysIcon,
-	},
-	{
-		name: "Security",
-		description: "Your customers’ data will be safe and secure",
-		href: "#",
-		icon: FingerPrintIcon,
-	},
-	{
-		name: "Integrations",
-		description: "Connect with third-party tools",
-		href: "#",
-		icon: SquaresPlusIcon,
-	},
-	{
-		name: "Automations",
-		description: "Build strategic funnels that will convert",
-		href: "#",
-		icon: ArrowPathIcon,
-	},
-];
-const callsToAction = [
-	{ name: "Watch demo", href: "#", icon: PlayCircleIcon },
-	{ name: "Contact sales", href: "#", icon: PhoneIcon },
-];
 
-function classNames(...classes: string[]) {
-	return classes.filter(Boolean).join(" ");
-}
 
 export default function Navigation() {
+    
+    const [bgColor, setBgColor] = useState("bg-main"); // set initial background color
+    const handleScroll = () => {
+        if (window.pageYOffset > 0) {
+            setBgColor("bg-main_2"); // change background color to blue when user scrolls down
+        } else {
+            setBgColor("bg-main"); // change back to gray when user is at the top
+        }
+    };
+
+    useEffect(() => {
+        window.addEventListener('scroll', handleScroll); // add scroll event listener
+        return () => {
+          window.removeEventListener('scroll', handleScroll); // remove event listener on cleanup
+        };
+      }, []);
+
+
+
 	const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
 
 	return (
-		<header className="bg-main sticky py-4 top-0 z-10 ">
+		<header className={`${bgColor} transition-colors duration-500 sticky py-4 top-0 z-10 `}>
 			<nav
 				className="px-8 md:px-16 lg:px-28 flex items-center justify-between p-6"
 				aria-label="Global"
