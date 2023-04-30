51c226f4b32527bdfea2e2706d84d917f458e6ef
Harman Singh <harman.singh@student.howest.be>
2023-04-30 15:54:37 +0200

    Alter text on About section

--- src/components/about/about.tsx
+++ src/components/about/about.tsx
@@ -2,26 +2,33 @@ import { FaLinkedin } from "react-icons/fa";
 
 export default function About() {
 	return (
-		<div id="ABOUT" className="bg-main items-center text-tan min-h-screen mb-64 px-8 md:px-16 lg:px-28">
+		<div
+			id="ABOUT"
+			className="bg-main items-center text-tan min-h-screen mb-64 px-8 md:px-16 lg:px-28"
+		>
 			<div className="lg:grid lg:grid-cols-2 pt-72 gap-4">
 				<div id="about-header">
 					<h2 className="text-2xl pb-4 text-accent">About</h2>
-					<p className=" text-4xl sm:text-5xl md:text-5xl lg:text-6xl xl:text-7xl font-dm max-w-[14ch]">
-						An inspiring headline about yourself.
-					</p>
+					<div className="text-4xl sm:text-5xl lg:text-6xl xl:text-7xl font-dm max-w-[14ch]">
+						<p>
+							&apos;&apos;ML aficionado&apos;&apos;
+						</p>
+						<span className="text-2xl sm:text-3xl lg:text-4xl xl:text-5xl">
+							- ChatGPT
+						</span>
+					</div>
 				</div>
-				<div className="text-xl sm:text-2xl 2xl:text-4xl font-ubuntu max-w-[40ch] font-light leading-10">
+				<div className="text-xl sm:text-2xl 2xl:text-4xl font-dm max-w-[40ch] font-light leading-10">
 					<p className="pt-12">
-						I&apos;m a second-year Computer Science major,
-						specializing in Artificial Intelligence.
+						I started programming in 2020
 					</p>
 					<p className="pt-8">
-						I started programming in May of 2020, after discovering
-						Harvard&apos;s CS50.
+						Currently I&apos;m a second-year Computer Science major,
+						specializing in Artificial Intelligence.
 					</p>
 				</div>
 			</div>
-			<div className="bg-accent mt-16 px-8 py-5 rounded-md max-w-fit text-tan text-md font-medium">
+			<div className="bg-accent mt-8 px-8 py-5 rounded-md max-w-fit text-tan text-md font-medium">
 				<a
 					href="https://www.linkedin.com/in/harmanpnahal/"
 					target="_blank"
@@ -34,8 +41,17 @@ export default function About() {
 			</div>
 
 			<div className="mt-64 text-white font-ubuntu font-bold  md:text-4xl text-center">
-				<p>This portfolio is not yet completed. Visit <a href="https://harmanpsingh.vercel.app/" title="My Current Portfolio" className="text-blue-500 hover:animate-pulse underline underline-offset-4">my current Portfolio</a> here	</p>
-				
+				<p>
+					This portfolio is not yet completed. Visit{" "}
+					<a
+						href="https://harmanpsingh.vercel.app/"
+						title="My Current Portfolio"
+						className="text-blue-500 hover:animate-pulse underline underline-offset-4"
+					>
+						my current Portfolio
+					</a>{" "}
+					here{" "}
+				</p>
 			</div>
 		</div>
 	);
